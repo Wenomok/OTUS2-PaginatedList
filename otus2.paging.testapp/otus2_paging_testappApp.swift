74699adbc83ed17817a7dbb67f62d06c98a30305
Vladislav Ivshin <wenomok@gmail.com>
2022-03-14 22:48:47 +0400

    Initial Commit

---
+++ otus2.paging.testapp/otus2_paging_testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  otus2_paging_testappApp.swift
+//  otus2.paging.testapp
+//
+//  Created by Vladislav Ivshin on 14.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct otus2_paging_testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
